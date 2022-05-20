0bf4122e52202bca0839a96f98fea5faff520d82
nicolaslop <102876666+nicolaslop@users.noreply.github.com>
2022-05-19 22:24:03 -0500

    cambios

--- abmclientes/index.php
+++ abmclientes/index.php
@@ -1,4 +1,40 @@
+<?php
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
+if(file_exists("archivo.txt")){
+    //si el archivo existe, cargo los dato en la variable $aClientes
+    $strjson = file_get_contents("archivo.txt");
+    $aClientes = json_decode($strjson , true);
+} else{
+    //si el archivo no existe es porque no hay cliente
+    $aClientes = array();
+}
 
+if($_POST){// si es postback almacenamos datos
+    $dni =$_POST["txtDni"];
+    $nombre =$_POST["txtNombre"];
+    $telefono =$_POST["txtTelefono"];
+    $correo =$_POST["txtCorreo"];
+
+    //creamos un array
+$aClientes=array();
+$aClientes[]=array(
+                    "dni"=>$dni,
+                    "nombre"=>$nombre,
+                    "telefono"=>$telefono,
+                    "correo"=>$correo,
+                );
+//convertimos el array de clientes en json
+//almacenamos en un archivo.txt el json.
+
+  $strjson = json_encode($aClientes);
+
+file_put_contents("archivo.txt", $strjson);
+ }
+ echo "actualizado";
+
+?>
 <!DOCTYPE html>
 <html lang="es">
 
@@ -6,7 +42,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>formulario datos personales</title>
+    <title>ABM CLIENTES</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
@@ -34,9 +70,10 @@
     
                         <label for="">correo:*</label>
                         <input type="email" name="txtCorreo" id="txtCorreo" class="form-control my-2"require>
+
                         <label for="">archivo adjunto</label>
-                        <input type="file" name="archivo" id="archivo" accept=".doc,.docx,.pdf">
-                        <p>archivos admitidos: "doc.docx.pdf"</p>
+                        <input type="file" name="archivo" id="archivo" accept=".jpg,.jpeg,.png">
+                        <p>archivos admitidos: "jpg.jpeg.png"</p>
                         
                         
                         <button type="submit" name="btnAgregar" class="btn bg-primary text-white mx-2">GUARDAR</button>
